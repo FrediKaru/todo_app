21c32eae0713aa2928fb207666d05de3bafe9d7a
Fredi Karu <fredikaru@gmail.com>
2023-07-11 19:46:44 +0300

    Intialize react app

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+const tasksToAdd = [
+  { title: "Washing maschine // black", id: 1, isUrgent: true },
+  { title: "Groceries", id: 2, isUrgent: false },
+  { title: "Walk with dog", id: 3, isUrgent: true },
+  { title: "Walk", id: 4, isUrgent: false },
+  { title: "Jog", id: 3, isUrgent: false },
+  { title: "Shop shoes", id: 3, isUrgent: true },
+  { title: "Jog", id: 3, isUrgent: false },
+  { title: "Jog", id: 3, isUrgent: false },
+  { title: "Training in sda", id: 3, isUrgent: true },
+  { title: "Cats and dogs", id: 3, isUrgent: false },
+  { title: "Jo", id: 3, isUrgent: false },
+  { title: "TASKS IN FED", id: 3, isUrgent: true },
+  { title: "g", id: 3, isUrgent: false },
+  { title: "Jog", id: 3, isUrgent: false },
+  { title: "Jg", id: 3, isUrgent: false },
+  { title: "Jog", id: 3, isUrgent: false },
+  { title: "Jog", id: 3, isUrgent: false },
+  { title: "dd", id: 3, isUrgent: false },
+];
 
+function List(props) {
+  const taskList = props.tasks.map((task) => (
+    <li style={{ color: task.isUrgent ? "red" : "green" }}>{task.title}</li>
+  ));
+  return <ul>{taskList}</ul>;
+}
+function UserInput({ tasks, setTasks }) {
+  function addTask() {
+    setTasks([...tasks, { title: "Dinosaur", id: 6 }]);
+  }
+  return (
+    <div>
+      <input type="text"></input>
+      <button onClick={addTask}>Submit</button>
+    </div>
+  );
+}
 function App() {
+  const [tasks, setTasks] = useState(tasksToAdd);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>To-do application</h1>
+        <UserInput tasks={tasks} setTasks={setTasks} />
+        <List tasks={tasks} />
       </header>
+      <body></body>
     </div>
   );
 }
